6f3e6d89fc396d752c8854309839f2231362f005
Guilherme Lionel <4542.lionel@gmail.com>
2025-02-28 16:34:32 -0300

    Numero Secreto

---
+++ app.js
@@ -0,0 +1,69 @@
+//definindo as variaveis
+let numeroSecreto, 
+    tentativas = 0,
+    chute = 0,
+    plural = 0,
+    jaPassou = [],
+    dificuldadeJogo = 10;
+
+//função escreve na tela
+function escreverTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+mensagemInicial();
+
+function verificarChute() {
+    console.log(numeroSecreto)
+    let chute = document.querySelector('input').value;
+
+    if (isNaN(chute) || chute < 1 || chute > dificuldadeJogo) {
+        escreverTela('h1', `Digite um número entre 1 e ${dificuldadeJogo}.`);
+        escreverTela('p', '')
+        return;
+    }
+
+    if (jaPassou.includes(chute)) {
+        escreverTela('h1', 'Você já tentou esse número. Tente outro.');
+        escreverTela('p', '')
+        return;
+    }
+
+    tentativas++;
+    jaPassou.push(chute);
+
+    if (chute == numeroSecreto) {
+        plural = tentativas !== 1 ? 'tentativas' : 'tentativa';
+        escreverTela('h1', `PARABÉNS, Você acertou com ${tentativas} ${plural}.`);
+        escreverTela('p', 'Clique em "Novo Jogo" para jogar novamente.');
+        document.getElementById("reiniciar").removeAttribute('disabled');
+    } else {
+        escreverTela('h1', 'ERROU...');
+        if (chute > numeroSecreto) {
+            escreverTela('p', 'O número secreto é menor.');
+        } else {
+            escreverTela('p', 'O número secreto é maior.');
+        }
+        document.querySelector('input').value = '';
+    }
+}
+
+function gerarNumero() {
+    return parseInt(Math.random() * dificuldadeJogo) + 1;
+}
+
+function mensagemInicial() {
+    numeroSecreto = gerarNumero();
+    escreverTela('h1', 'Jogo do Secreto.');
+    escreverTela('p', `Digite um número entre 1 e ${dificuldadeJogo}.`);
+}
+
+function novoJogo() {
+    numeroSecreto = gerarNumero();
+    tentativas = 0;
+    jaPassou = [];
+    document.querySelector('input').value = '';
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
